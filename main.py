0f2e2cb6ffcdf50770f8b1b1e59dbbd287e84d5c
Mijash Ghimire <76825805+gmijash@users.noreply.github.com>
2022-03-04 11:16:51 -0800

    Changes for AWS

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+from email.mime import application
 from website import create_app
 
+app = application
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0')
+    app.run(debug=True,)
