1fca3ff9ca7cf1d73e5d5b6cb5c9d52fbdfb7c0a
Abhishek Maharjan <abhishek.maharjan@pluto.tv>
2022-03-09 16:34:25 -0800

    working app

--- application.py
+++ application.py
@@ -4,4 +4,4 @@ from website import create_app
 application = create_app()
 
 if __name__ == '__main__':
-    application.run(host ='0.0.0.0', port = 5013, debug=True,)
+    application.run(host='0.0.0.0', port= 5013, debug=True,)
